47f52528b6dd428959e5f8ec69a24c10834aab62
Sakurai Youhei <youhei.sakurai@ysoft.com>
2014-03-10 23:00:23 +0900

    Most probably even expect works fine but very slow...

--- jvncdotool/fakepilimage.py
+++ jvncdotool/fakepilimage.py
@@ -1,3 +1,4 @@
+import time
 
 #import pymaging_bmp.bmp
 import pymaging_png.png
@@ -9,7 +10,23 @@ pymaging.formats.register(pymaging_jpg.jpg.JPG)
 
 import pymaging.image
 
-class Image(pymaging.image.Image):
+class Image(pymaging.image.LoadedImage):
+    @classmethod
+    def convertImage(cls, img):
+        img = cls(img.mode, img.width, img.height, img.pixels, img.palette)
+        img.size = (img.width, img.height)
+        #pymaging.image.Image.save(img, open("%s.png"%time.time(), "wb"), "png")
+        return img
+
+    @classmethod
+    def open(cls, file, *args):
+        if len(args):
+            mode = args[0]
+            img = pymaging.image.LoadedImage.open(open(file, mode))
+        else:
+            img = pymaging.image.LoadedImage.open(open(file, "rb"))
+        return cls.convertImage(img)
+
     def save(self, outfile):
         format = None
         if outfile.lower().endswith(".png"):
@@ -20,7 +37,7 @@ class Image(pymaging.image.Image):
             format = "bmp"
         if not format:
           raise Error("Unknown image format was detected: %s"%outfile)
-        return pymaging.image.Image.save(open(outfile, "wb"), format)
+        return pymaging.image.Image.save(self, open(outfile, "wb"), format)
 
     @classmethod
     def fromstring(cls, mode, size, data, *args):
@@ -49,10 +66,44 @@ class Image(pymaging.image.Image):
             raise Error("Unexpected decode mode: decodemode->%s"%decodemode)
           
         pixels = get_pixel_array(image_bytes, size[0], size[1], 3)
-        img = Image.LoadedImage(RGB, size[0], size[1], pixels)
+        img = cls(RGB, size[0], size[1], pixels)
         img.size = size
         return img
     
     def paste(self, *args):
         import logging
-        logging.warn("Do nothing because paste() is not implementated, yet: %s"%args)
+        if not issubclass(args[0].__class__, pymaging.image.Image):
+            logging.warn("Do nothing because only Image update is implemented in paste().")
+        else:
+            if len(args)==3:
+                logging.warn("Mask will be ignored: %s"%args[2])
+            img = args[0]
+            box = args[1]
+            if box is None:
+                box = (0, 0)
+            elif len(box)==4:
+                img.resize(box[0]-box[2], box[1]-box[3])
+                box = (box[0], box[1])
+            if len(box)!=2:
+                raise Error("Unexpected box was given: %s"%box)
+            for x in xrange(img.width):
+                for y in xrange(img.height):
+                    self.set_color(box[0]+x, box[1]+y, img.get_color(x, y))
+            
+    def histogram(self):
+        histo = [0,]*(255*self.pixelsize)
+        for x in xrange(self.width):
+            for y in xrange(self.height):
+                col = self.get_color(x, y)
+                histo[col.red] +=1
+                histo[256+col.green] +=1
+                histo[256+256+col.blue] +=1
+                if self.pixelsize==4:
+                    histo[256+256+256+col.alpha] +=1
+        return histo
+
+    def crop(self, box):
+        img = pymaging.image.Image.crop(self, box[2]-box[0], box[3]-box[1], box[1], box[0])
+        return Image.convertImage(img)
+
+
